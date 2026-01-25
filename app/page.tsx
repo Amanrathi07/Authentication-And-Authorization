5f7ac2bbf4180e3986c047602f30c9f9adea38fa
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-25 17:58:42 +0530

    simple signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,38 @@
 ""
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 
 export default function Home() {
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+
+  async  function formHandel(){
+    await authClient.signUp.email({
+      name,
+      email,
+      password,
+    },{
+      onSuccess:()=>{
+        alert("user is created")
+      },
+      onError:(ctx)=>{
+        alert(ctx.error.message)
+      }
+    })
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <div>
-      <Input placeholder="name" />
-      <Input placeholder="email" />
-      <Input placeholder="password" />
+     <div className="flex flex-col gap-4">
+      <Input placeholder="name" onChange={(e)=>setName(e.target.value)} />
+      <Input placeholder="email" onChange={(e)=>setEmail(e.target.value)} />
+      <Input placeholder="password" onChange={(e)=>setPassword(e.target.value)}/>
 
+      <Button onClick={formHandel}>create user</Button>
      </div>
     </div>
   );
