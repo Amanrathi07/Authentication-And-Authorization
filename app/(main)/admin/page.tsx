9b09e78b9757035dae959063c33256b8a7484be4
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-30 23:13:48 +0530

    unauthorized() function

--- app/(main)/admin/page.tsx
+++ app/(main)/admin/page.tsx
@@ -1,8 +1,13 @@
+import { getServerSession } from "@/lib/getServerSession";
+import { redirect, unauthorized } from "next/navigation";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+    const session= await getServerSession();
+    if(!session?.user)unauthorized()
+
   return (
     <div className="flex flex-col gap-6 min-h-screen items-center justify-center bg-zinc-50 px-4 dark:bg-black">
-            admin page 
+            you have admin access
     </div>
   )
 }
