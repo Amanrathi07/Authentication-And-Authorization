20c8d7cd815f36e2fcb35f7087d6d6a730cb7e59
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-30 23:06:43 +0530

    added adminPage

---
+++ app/(main)/admin/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AdminPage() {
+  return (
+    <div className="flex flex-col gap-6 min-h-screen items-center justify-center bg-zinc-50 px-4 dark:bg-black">
+            admin page 
+    </div>
+  )
+}
