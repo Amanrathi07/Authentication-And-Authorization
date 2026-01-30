ad7d88e661599d2efe54dbb1bd46efe77c1ad880
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-30 23:18:58 +0530

    forbidden() function

--- app/(main)/admin/page.tsx
+++ app/(main)/admin/page.tsx
@@ -1,9 +1,14 @@
 import { getServerSession } from "@/lib/getServerSession";
-import { redirect, unauthorized } from "next/navigation";
+import { forbidden, unauthorized } from "next/navigation";
 
 export default async function AdminPage() {
     const session= await getServerSession();
-    if(!session?.user)unauthorized()
+    if(!session?.user){
+        return unauthorized()
+    }
+    if(session?.user.role!=="admin"){
+        return forbidden();
+    } 
 
   return (
     <div className="flex flex-col gap-6 min-h-screen items-center justify-center bg-zinc-50 px-4 dark:bg-black">
