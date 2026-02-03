12479b051a9ee30c34709e3fd527339886aa3784
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-03 22:58:38 +0530

    password reset working

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -2,16 +2,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function page() {
     const [password,setPassword] = useState("")
     const [confPassword,setConfPassword] = useState("")
 
+    const searchParams = useSearchParams();
+    const token = searchParams.get("token");
+
     async function onSubmit() {
         if(password!==confPassword) return
+        if(!token) return 
 
-        // const {error} = await authClient.
+       const {error}=await authClient.resetPassword({
+          newPassword:password,
+          token ,          
+        })
+        if(error){
+          toast.error(error.message || "somthing went worng")
+        }else{
+          toast.success("password reset sussesful") ;
+          useRouter().push("/sign-in")
+        }
+
+        
     }
   return (
     <div className="h-screen w-screen flex items-center justify-center">
