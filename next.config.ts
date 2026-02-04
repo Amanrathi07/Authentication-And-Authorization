f10effa3a376b8d3f0e2ae84975e69d091150a32
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-04 22:22:46 +0530

    images work

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     remotePatterns:[
       {
         protocol:"https",
-        hostname:"h3.googleusercontent.com"
+        hostname:"h3.googleusercontent.com  "
       }
     ]
   }
