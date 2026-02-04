0795e4b508904364a5c45b0f32819dc7a335fac1
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-04 22:21:14 +0530

    image url added

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
   },
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:"h3.googleusercontent.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
