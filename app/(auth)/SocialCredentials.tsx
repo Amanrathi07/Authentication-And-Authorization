93653f9a4c30551be6ab54eb32a97735dfefaf56
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-05 22:42:47 +0530

    icons change

--- app/(auth)/SocialCredentials.tsx
+++ app/(auth)/SocialCredentials.tsx
@@ -3,7 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import toast from "react-hot-toast";
-import { Chrome, Github } from "lucide-react";
+import { FaGithub } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 
 export function SocialCredentials() {
 
@@ -33,7 +34,7 @@ export function SocialCredentials() {
         className="flex items-center gap-2"
         onClick={() => socialHandel("google")}
       >
-        <Chrome className="h-4 w-4" />
+        <FaGoogle className="h-4 w-4" />
         Continue with Google
       </Button>
 
@@ -42,7 +43,7 @@ export function SocialCredentials() {
         className="flex items-center gap-2"
         onClick={() => socialHandel("github")}
       >
-        <Github className="h-4 w-4" />
+        <FaGithub className="h-4 w-4" />
         Continue with GitHub
       </Button>
     </div>
