35f79e40519ab39fe161b8b093b8f758a1282de0
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-08 22:36:18 +0530

    edit logic

--- app/(main)/profile/ProfilePage.tsx
+++ app/(main)/profile/ProfilePage.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
 
 interface Props {
   user: {
@@ -17,6 +21,12 @@ interface Props {
 }
 
 export default function ProfilePage({ user }: Props) {
+    const [edit,setEdit]=useState(false)
+    const [name,setName] = useState(user.name);
+    async function handelClick(){
+
+    }
+
   return (
     <div className="flex min-h-screen items-center justify-center px-4">
       <Card className="w-full max-w-md">
@@ -31,9 +41,10 @@ export default function ProfilePage({ user }: Props) {
                 .toUpperCase()}
             </AvatarFallback>
           </Avatar>
-
-          <CardTitle className="text-xl">{user.name}</CardTitle>
-
+              
+            {(edit)?<Input className="text-xl" value={name} type="text" onChange={(e)=>setName(e.target.value)} / >:
+          <CardTitle  className="text-xl">{user.name}</CardTitle>}
+          
           <Badge variant="secondary" className="capitalize">
             {user.role}
           </Badge>
@@ -75,7 +86,11 @@ export default function ProfilePage({ user }: Props) {
           <Separator />
 
           <div className="flex justify-end">
-            <Button variant="outline">Edit Profile</Button>
+            {(edit)?<div className="flex gap-8">
+            <Button onClick={()=>setEdit(v=>!v)} variant={"destructive"}>cancel</Button>
+            <Button className="bg-green-400 hover:bg-green-400 hover:scale-105" >save</Button>
+            </div>:
+            <Button variant="outline" onClick={()=>setEdit(v=>!v)}>Edit Profile</Button>}
           </div>
         </CardContent>
       </Card>
